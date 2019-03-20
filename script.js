fb8c7f5d9a8f6d26b2a96ee6649c67a655a94243
jaimie <moore.jaimie@gmail.com>
2019-03-20 13:16:43 -0400

    responsive, hamburger menu

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ $('a').smoothScroll({
     offset: 100,
     autoFocus: false
 });
+
+$(document).ready(function () {
+    $('.hamburger').on('click', function () {
+        $('.siteNav').toggleClass('show');
+    });
+
+    $('li').on('click', function () {
+        $('.siteNav').toggleClass('hide');
+    })
+
+    $('.exitResponsive').on('click', function () {
+        $('.siteNav').toggleClass('show');
+    });
+});
